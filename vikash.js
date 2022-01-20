b153758ca103e5e50221809c3469e44ea189a3b4
Sakshi Sharma <sakshi.sharma14199@gmail.com>
2022-01-20 12:27:20 +0530

    tt-2

---
+++ vikash.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class vikash extends Component {
+  static propTypes = {
+    second: third,
+  };
+
+  render() {
+    return <div>
+        vikash sirrrr
+    </div>;
+  }
+}
